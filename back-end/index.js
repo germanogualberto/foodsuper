f448940959abf0350db3918f057e0140c4fc0ba5
Germano Gualberto <gmanopoliano@gmail.com>
2018-10-18 08:36:34 -0300

    rotas das paginas no servidor

--- back-end/index.js
+++ back-end/index.js
@@ -11,3 +11,25 @@ const port = app.get('port');
 http.createServer(app).listen(port, () => {
   console.log(`Express Server listening on port ${port}`);
 });
+
+app.get('/', (req, res) => {
+  res.sendFile('./front-end/index.html', { root: __dirname });
+});
+app.get('/index.html', (req, res) => {
+  res.sendFile('./front-end/index.html', { root: __dirname });
+});
+app.get('/addclient.html', (req, res) => {
+  res.sendFile('./front-end/addclient.html', { root: __dirname });
+});
+app.get('/addsupermercado.html', (req, res) => {
+  res.sendFile('./front-end/addsupermercado.html', { root: __dirname });
+});
+app.get('/login.html', (req, res) => {
+  res.sendFile('./front-end/login.html', { root: __dirname });
+});
+app.get('/main.html', (req, res) => {
+  res.sendFile('./front-end/main.html', { root: __dirname });
+});
+app.get('/updateclient.html', (req, res) => {
+  res.sendFile('./front-end/updateclient.html', { root: __dirname });
+});
