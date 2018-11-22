23ecd98067bffe14a69267caff9c94611c568b60
Germano Gualberto <gmanopoliano@gmail.com>
2018-11-22 00:38:26 -0300

    refatoramento de paginas e modelagem produto

---
+++ back-end/app/controllers/product.js
@@ -0,0 +1,127 @@
+module.exports = (app) => {
+  const Product = app.models.product;
+
+  const controller = {};
+
+  /**
+   * List all products.
+   * @param {*} req
+   * @param {*} res
+   * @return {Array} products
+   */
+  controller.list = (req, res) => {
+    Product.find({})
+      .sort({ name: 1 })
+      .lean(true)
+      .exec((error, products) => {
+        if (error) {
+          console.log(`error: ${error}`);
+          return res.status(500).json(error);
+        }
+        return res.status(200).json(products);
+      });
+  };
+
+  /**
+   * Return product by _id.
+   * @param {*} req
+   * @param {*} res
+   * @return {Object} product
+   */
+  controller.get = (req, res) => {
+    const { _id } = req.params;
+    Product.findOne({ _id })
+      .lean(true)
+      .exec((error, prod) => {
+        if (error) {
+          console.log(`error: ${error}`);
+          return res.status(500).json(error);
+        }
+        return res.status(200).json(prod);
+      });
+  };
+
+  /**
+   * Add a product with passed properties
+   * @param {*} req
+   * @param {*} res
+   * @return {Object} product
+   */
+  controller.add = (req, res) => {
+    const {
+      name,
+      price,
+      idSupermercado
+    } = req.body;
+
+    const newProduct = new Product();
+    newProduct.name = name;
+    newProduct.price = price;
+    newProduct.idSupermercado = idSupermercado;
+
+    newProduct.save((error, prod) => {
+      if (error) {
+        console.log(`error: ${error}`);
+        const errorToReturn = {};
+        if (error.code === 1100) {
+          errorToReturn.message = 'Já existe um produto com esse id.';
+        } else {
+          errorToReturn.message =
+            'Não foi possível criar produto.' +
+            'Consulte um desenvolvedor do app.';
+        }
+        return res.status(500).json(errorToReturn);
+      }
+
+      return res.status(200).json(prod);
+    });
+  };
+
+  /**
+   * Update product by id
+   * @param {*} req
+   * @param {*} res
+   * @return {Object} product updated
+   */
+  controller.update = (req, res) => {
+    const {
+      name,
+      price,
+      idSupermercado
+    } = req.body;
+    const data = {};
+    if (name) data.name = name;
+    if (price) data.price = price;
+    if (idSupermercado) data.idSupermercado = idSupermercado;
+
+    const { _id } = req.params;
+
+    Product.findOneAndUpdate({ _id }, data, { new: true })
+      .lean(true)
+      .exec((error, product) => {
+        if (error) {
+          console.log(`error: ${error}`);
+          return res.status(500).json(error);
+        }
+        return res.status(200).json(product);
+      });
+  };
+
+  /**
+   * Delete product by id
+   * @param {*} req
+   * @param {*} res
+   */
+  controller.delete = (req, res) => {
+    const { _id } = req.params;
+
+    Product.findByIdAndDelete(_id).exec((error) => {
+      if (error) {
+        console.log(`error: ${error}`);
+        return res.status(500).json(error);
+      }
+    });
+  };
+
+  return controller;
+};
---
+++ back-end/app/routes/product.js
@@ -0,0 +1,25 @@
+module.exports = (app) => {
+  const controller = app.controllers.product;
+  const permissionUtils = app.utils.permission;
+
+  const updateAndDeleteP = [permissionUtils.userTypes.SUPERMARKET];
+
+  app
+    .route('/product')
+    .post(controller.add)
+    .get(controller.list);
+
+  app
+    .route('/product/:id')
+    .get(controller.get)
+    .put(
+      permissionUtils.isLoggedIn,
+      permissionUtils.roleAuthorization(updateAndDeleteP),
+      controller.update
+    )
+    .delete(
+      permissionUtils.isLoggedIn,
+      permissionUtils.roleAuthorization(updateAndDeleteP),
+      controller.delete
+    );
+};
