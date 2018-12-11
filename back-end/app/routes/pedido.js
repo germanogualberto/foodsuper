949a85d117c4c2506d617caa52e0951f0a352d52
Germano Gualberto <gmanopoliano@gmail.com>
2018-12-11 00:25:35 -0300

    route pedido criado

---
+++ back-end/app/routes/pedido.js
@@ -0,0 +1,39 @@
+module.exports = (app) => {
+  const controller = app.controllers.pedido;
+  const permissionUtils = app.utils.permission;
+
+  const updateAndDeleteP = [permissionUtils.userTypes.SUPERMARKET];
+
+  app
+    .route('/pedido')
+    .post(controller.add)
+    .get(controller.list);
+
+  app
+    .route('/pedido/:idSupermercado')
+    .get(controller.get1)
+    .put(
+      permissionUtils.isLoggedIn,
+      permissionUtils.roleAuthorization(updateAndDeleteP),
+      controller.update
+    )
+    .delete(
+      permissionUtils.isLoggedIn,
+      permissionUtils.roleAuthorization(updateAndDeleteP),
+      controller.delete
+    );
+
+    app
+    .route('/pedido/:idCliente')
+    .get(controller.get2)
+    .put(
+      permissionUtils.isLoggedIn,
+      permissionUtils.roleAuthorization(updateAndDeleteP),
+      controller.update
+    )
+    .delete(
+      permissionUtils.isLoggedIn,
+      permissionUtils.roleAuthorization(updateAndDeleteP),
+      controller.delete
+    );
+};
